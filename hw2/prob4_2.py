538f737ffab438f77244a3ab831b2c4d0258ef87
Ryan George <ryan@rgeorge.net>
2018-02-27 20:35:05 -0500

    finishing hw2

--- hw2/prob4_2.py
+++ hw2/prob4_2.py
@@ -17,6 +17,8 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
 
 import tensorflow as tf
 
+import numpy as np
+
 # Parameters
 learning_rate = 0.1
 num_steps = 1000
@@ -29,17 +31,21 @@ n_hidden_2 = 256 # 2nd layer number of neurons
 num_input = 784 # MNIST data input (img shape: 28*28)
 num_classes = 10 # MNIST total classes (0-9 digits)
 
+keep_prob = .2
+dpt = lambda x: tf.contrib.layers.dropout(x, keep_prob=keep_prob)
 
 # Define the neural network
 def neural_net(x_dict):
     # TF Estimator input is a dict, in case of multiple inputs
     x = x_dict['images']
     # Hidden fully connected layer with 256 neurons
-    layer_1 = tf.layers.dense(x, n_hidden_1)
+    
+
+    layer_1 = tf.layers.dense(dpt(x), n_hidden_1)
     # Hidden fully connected layer with 256 neurons
-    layer_2 = tf.layers.dense(layer_1, n_hidden_2)
+    layer_2 = tf.layers.dense(dpt(layer_1), n_hidden_2)
     # Output fully connected layer with a neuron for each class
-    out_layer = tf.layers.dense(layer_2, num_classes)
+    out_layer = tf.layers.dense(dpt(layer_2), num_classes)
     return out_layer
 
 
@@ -61,10 +67,23 @@ def model_fn(features, labels, mode):
     # Compare the use of squared loss, cross entropy loss, and softmax with log-likelihood 
     
     onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
-    
-    loss_op = tf.losses.mean_squared_error(labels=onehot_labels, predictions=pred_probas)
+
+    regularization_style = 'none'
+
+    if regularization_style == 'l1':
+        reg = tf.contrib.layers.apply_regularization(
+            tf.contrib.layers.l1_regularizer(scale=1.0),
+            weights_list=tf.trainable_variables()[::2])
+    elif regularization_style == 'l2':
+        reg = tf.contrib.layers.apply_regularization(
+            tf.contrib.layers.l2_regularizer(scale=.005),
+            weights_list=tf.trainable_variables()[::2])
+    else:
+        reg = 0
+
+    # loss_op = tf.losses.mean_squared_error(labels=onehot_labels, predictions=pred_probas)
     # loss_op = tf.losses.log_loss(labels=onehot_labels, predictions=pred_probas)
-    # loss_op = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
+    loss_op = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits) + reg
 
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
     train_op = optimizer.minimize(
